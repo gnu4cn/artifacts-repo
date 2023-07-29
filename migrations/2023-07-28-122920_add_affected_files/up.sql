0e679bf5aefb81de262c69eec65de70dc219d55c
Peng Hailin, <unisko@gmail.com>
2023-07-29 08:37:54 +0800

    Refactored.

--- migrations/2023-07-28-122920_add_affected_files/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TYPE edit_type AS ENUM ('add', 'edit', 'delete');
-
-CREATE TABLE affected_files (
-    affected_file_id SERIAL PRIMARY KEY,
-    file_edit_type edit_type NOT NULL,
-    file_path VARCHAR(511) NOT NULL,
-    release_id INTEGER NOT NULL REFERENCES releases(release_id)
-);
