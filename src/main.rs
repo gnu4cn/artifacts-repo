7df32233c6adead6f669cc9350bb6aa9e911a403
Peng Hailin, <unisko@gmail.com>
2023-07-29 14:53:13 +0800

    Refactored.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!\n")
-}
+pub mod handlers;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
-            .service(greet)
+            .route("/hello", web::get().to(|| async { "Hello World!\n" }))
+            .service(handlers::greet)
     })
-    .bind(("127.0.0.1", 10080))?
+    .bind(("127.0.0.1", 20080))?
         .run()
         .await
 }
