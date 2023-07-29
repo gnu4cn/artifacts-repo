21e70c9abc9a142964d65297063bdea1ae8251d3
Peng Hailin, <unisko@gmail.com>
2023-07-29 08:39:08 +0800

    Refactored.

--- sql-files/artifacts-down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE artifacts;
-DROP TYPE IF EXISTS size_unit;
--- sql-files/releases-down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE releases;
-DROP TYPE channel_type;
