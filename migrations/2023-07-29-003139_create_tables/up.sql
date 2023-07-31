7ad2fa57128cd2d914f5d0104871c8d54f9889d7
rust-lang.xfoss.com <unisko@gmail.com>
2023-07-31 16:17:02 +0800

    Change id to UUID.

--- migrations/2023-07-29-003139_create_tables/up.sql
+++ migrations/2023-07-29-003139_create_tables/up.sql
@@ -2,7 +2,7 @@
 CREATE TYPE channel_type AS ENUM ('nightly', 'beta', 'stable');
 
 CREATE TABLE releases (
-    release_id SERIAL PRIMARY KEY,
+    release_id VARCHAR(255) PRIMARY KEY,
     channel channel_type NOT NULL DEFAULT 'nightly',
     repo_fullname VARCHAR(255) NOT NULL,
     diffs_url VARCHAR(512),
@@ -11,12 +11,12 @@ CREATE TABLE releases (
 
 -- Create table changelogs;
 CREATE TABLE changelogs (
-    changelog_id SERIAL PRIMARY KEY,
+    changelog_id VARCHAR(255) PRIMARY KEY,
     commit_id CHAR(7) NOT NULL,
     commited_at TIMESTAMP NOT NULL,
     commit_comment VARCHAR(2048) NOT NULL,
     commited_by VARCHAR(255) NOT NULL,
-    release_id INTEGER NOT NULL REFERENCES releases(release_id)
+    release_id VARCHAR(255) NOT NULL REFERENCES releases(release_id)
 );
 
 
@@ -24,11 +24,11 @@ CREATE TABLE changelogs (
 CREATE TYPE size_unit AS ENUM ('kb', 'mb', 'gb');
 
 CREATE TABLE artifacts (
-    artifact_id SERIAL PRIMARY KEY,
+    artifact_id VARCHAR(255) PRIMARY KEY,
     filename VARCHAR(255) NOT NULL,
     filesize NUMERIC(4, 2) NOT NULL,
     filesize_unit size_unit NOT NULL,
-    release_id INTEGER NOT NULL REFERENCES releases(release_id)
+    release_id VARCHAR(255) NOT NULL REFERENCES releases(release_id)
 );
 
 
@@ -36,8 +36,8 @@ CREATE TABLE artifacts (
 CREATE TYPE edit_type AS ENUM ('add', 'edit', 'delete');
 
 CREATE TABLE affected_files (
-    affected_file_id SERIAL PRIMARY KEY,
+    affected_file_id VARCHAR(255) PRIMARY KEY,
     file_edit_type edit_type NOT NULL,
     file_path VARCHAR(511) NOT NULL,
-    release_id INTEGER NOT NULL REFERENCES releases(release_id)
+    release_id VARCHAR(255) NOT NULL REFERENCES releases(release_id)
 );
--- sql-files/up.sql
+++ sql-files/up.sql
@@ -2,23 +2,21 @@
 CREATE TYPE channel_type AS ENUM ('nightly', 'beta', 'stable');
 
 CREATE TABLE releases (
-    release_id SERIAL PRIMARY KEY,
+    release_id VARCHAR(255) PRIMARY KEY,
     channel channel_type NOT NULL DEFAULT 'nightly',
     repo_fullname VARCHAR(255) NOT NULL,
-    diffs_url VARCHAR(512) NOT NULL,
-    released_at DATE NOT NULL DEFAULT CURRENT_DATE,
-    CONSTRAINT release_pkey PRIMAY KEY (release_id)
+    diffs_url VARCHAR(512),
+    released_at DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
 -- Create table changelogs;
 CREATE TABLE changelogs (
-    changelog_id SERIAL PRIMARY KEY,
+    changelog_id VARCHAR(255) PRIMARY KEY,
     commit_id CHAR(7) NOT NULL,
     commited_at TIMESTAMP NOT NULL,
     commit_comment VARCHAR(2048) NOT NULL,
     commited_by VARCHAR(255) NOT NULL,
-    release_id INTEGER NOT NULL REFERENCES releases(release_id),
-    CONSTRAINT changelog_pkey PRIMAY KEY (changelog_id)
+    release_id VARCHAR(255) NOT NULL REFERENCES releases(release_id)
 );
 
 
@@ -26,12 +24,11 @@ CREATE TABLE changelogs (
 CREATE TYPE size_unit AS ENUM ('kb', 'mb', 'gb');
 
 CREATE TABLE artifacts (
-    artifact_id SERIAL PRIMARY KEY,
+    artifact_id VARCHAR(255) PRIMARY KEY,
     filename VARCHAR(255) NOT NULL,
     filesize NUMERIC(4, 2) NOT NULL,
     filesize_unit size_unit NOT NULL,
-    release_id INTEGER NOT NULL REFERENCES releases(release_id),
-    CONSTRAINT artifact_pkey PRIMARY KEY (artifact_id)
+    release_id VARCHAR(255) NOT NULL REFERENCES releases(release_id)
 );
 
 
@@ -39,9 +36,8 @@ CREATE TABLE artifacts (
 CREATE TYPE edit_type AS ENUM ('add', 'edit', 'delete');
 
 CREATE TABLE affected_files (
-    affected_file_id SERIAL PRIMARY KEY,
+    affected_file_id VARCHAR(255) PRIMARY KEY,
     file_edit_type edit_type NOT NULL,
     file_path VARCHAR(511) NOT NULL,
-    release_id INTEGER NOT NULL REFERENCES releases(release_id),
-    CONSTRAINT affected_file_pkey PRIMARY KEY (affected_file_id)
+    release_id VARCHAR(255) NOT NULL REFERENCES releases(release_id)
 );
