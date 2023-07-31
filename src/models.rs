533d3d0fc0689de5877b44efe18f580fe705e0c2
Lenny PENG <lenny.peng@senscomm.com>
2023-07-31 17:43:44 +0800

    Writing src/models.rs

--- src/models.rs
+++ src/models.rs
@@ -2,6 +2,30 @@ use serde::Serialize;
 
 use crate::schema::{releases, changelogs, artifacts, affected_files};
 
+#[derive(diesel_derive_enum::DbEnum)]
+#[ExistingTypePath = "crate::schema::sql_types::ChannelType"]
+pub enum ChannelType {
+    Nightly,
+    Beta,
+    Stable,
+}
+
+#[derive(diesel_derive_enum::DbEnum)]
+#[ExistingTypePath = "crate::schema::sql_types::EditType"]
+pub enum ChannelType {
+    Add,
+    Delete,
+    Edit,
+}
+
+#[derive(diesel_derive_enum::DbEnum)]
+#[ExistingTypePath = "crate::schema::sql_types::SizeUnit"]
+pub enum ChannelType {
+    Kb,
+    Mb,
+    Gb,
+}
+
 #[derive(Serialize)]
 pub struct GenericResponse {
     pub status: String,
