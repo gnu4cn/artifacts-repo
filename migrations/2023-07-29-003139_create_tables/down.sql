716a28fc22aa57c0ce0a24e8aa786999183b4366
Lenny PENG <lenny.peng@senscomm.com>
2023-09-11 14:40:24 +0800

    Preparing the 'tags' feature.

--- migrations/2023-07-29-003139_create_tables/down.sql
+++ migrations/2023-07-29-003139_create_tables/down.sql
@@ -2,5 +2,6 @@
 DROP TABLE IF EXISTS artifacts;
 DROP TABLE IF EXISTS affected_files;
 DROP TABLE IF EXISTS changelogs;
+DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS releases;
 DROP TABLE IF EXISTS repositories;
--- migrations/2023-07-29-003139_create_tables/up.sql
+++ migrations/2023-07-29-003139_create_tables/up.sql
@@ -5,12 +5,31 @@ CREATE TABLE repositories (
     repo VARCHAR(255) NOT NULL
 );
 
+-- Create table tags;
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    commit_id VARCHAR(48) NOT NULL,
+    release_id SERIAL NOT NULL REFERENCES releases(id),
+    repository_id SERIAL NOT NULL REFERENCES repositories(id)
+);
+
+
 -- Create table releases;
 CREATE TABLE releases (
     id SERIAL PRIMARY KEY NOT NULL,
     release_channel VARCHAR(255) NOT NULL,
     diffs_url VARCHAR(512),
     released_at DATE NOT NULL DEFAULT CURRENT_DATE,
+    repository_id SERIAL NOT NULL REFERENCES repositories(id)
+);
+
+-- Create table tags;
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    commit_id VARCHAR(48) NOT NULL,
+    release_id SERIAL NOT NULL REFERENCES releases(id),
     repository_id SERIAL NOT NULL REFERENCES repositories(id)
 );
 
