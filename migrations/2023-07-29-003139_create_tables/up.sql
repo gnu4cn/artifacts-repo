90a0a57d41cab4692632f58fd936299c46c577ba
Lenny PENG <lenny.peng@senscomm.com>
2023-09-11 14:45:19 +0800

    Preparing the 'tags' feature.

--- migrations/2023-07-29-003139_create_tables/up.sql
+++ migrations/2023-07-29-003139_create_tables/up.sql
@@ -5,16 +5,6 @@ CREATE TABLE repositories (
     repo VARCHAR(255) NOT NULL
 );
 
--- Create table tags;
-CREATE TABLE tags (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    commit_id VARCHAR(48) NOT NULL,
-    release_id SERIAL NOT NULL REFERENCES releases(id),
-    repository_id SERIAL NOT NULL REFERENCES repositories(id)
-);
-
-
 -- Create table releases;
 CREATE TABLE releases (
     id SERIAL PRIMARY KEY NOT NULL,
